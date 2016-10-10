7195e660cec386c9b1241ab7ace428bf46873cbb
howardmann <howardmann27@gmail.com>
2016-10-10 21:02:54 +1100

    changed ordering of add and delete items and debts

--- app/assets/javascripts/components/cart/MainCart.js.jsx
+++ app/assets/javascripts/components/cart/MainCart.js.jsx
@@ -9,10 +9,7 @@ class MainCart extends React.Component{
     }
   }
 
-  componentWillMount(){
-    this.fetchServer();
-  }
-
+  // Fetch json data and store in state before rendering components
   fetchServer(){
     $.ajax({
       url: `/carts/${this.props.current_cart}.json`,
@@ -27,6 +24,23 @@ class MainCart extends React.Component{
     });
   }
 
+  componentWillMount(){
+    this.fetchServer();
+  }
+
+  // Sync State with Server in intervals calling the handleSync method
+  componentDidMount(){
+    let syncTimer = setInterval(()=> {this.handleSync();},10000);
+    this.setState({syncTimer});
+  }
+
+  // Clear timer when leaving page
+  componentWillUnmount(){
+    clearInterval(this.state.syncTimer);
+    console.log('Cleared');
+  }
+
+  // Render NewItems/Debts passing in updateAdd state callbacks. Render AllItems/ AllDebts passing in state props and updateDelete callbacks
   render(){
     return (
       <div>
@@ -34,30 +48,30 @@ class MainCart extends React.Component{
         <NewItem updateAdd={this.updateAdd.bind(this)}/>
         <br/>
         <AllItems items={this.state.items} cartTotal={this.state.cartTotal} updateDelete={this.updateDelete.bind(this)}/>
-
         <hr/>
         <NewDebt updateAdd={this.updateAdd.bind(this)}/>
         <br/>
         <AllDebts debts={this.state.debts}
           updateDelete={this.updateDelete.bind(this)}/>
-
         <button className="btn new-bill" onClick={this.navigateBillNew.bind(this)}>New Bill</button>
       </div>
     )
   }
 
+  // Dynamically update state when item/debts are added
   updateAdd(property, response){
-    var newState = this.state[property].concat(response);
+    this.state[property].unshift(response);
 
     this.setState({
-      [property]:newState
+      [property]:this.state[property]
     });
 
     if (property === 'items') {
-      this.updateCartTotal(newState);
+      this.updateCartTotal(property);
     }
   }
 
+  // Dynamically update state when item/debts are deleted. Use JS filter method
   updateDelete(property, id){
     var newState = this.state[property].filter((el)=>{
       return el.id != id;
@@ -68,12 +82,13 @@ class MainCart extends React.Component{
     });
 
     if (property === 'items') {
-      this.updateCartTotal(newState);
+      this.updateCartTotal(property);
     }
   }
 
-  updateCartTotal(newState){
-    let newCartTotal = newState.reduce(function(sum,el){
+  // Callback to update cartTotal when item is modified
+  updateCartTotal(property){
+    let newCartTotal = this.state[property].reduce(function(sum,el){
       return sum+= parseInt(el.subtotal);
     },0).toFixed(2);
 
@@ -82,17 +97,7 @@ class MainCart extends React.Component{
     });
   }
 
-  navigateBillNew(){
-    this.handleSync();
-    if (this.state.items.length === 0){
-      alert("Please add items");
-    } else if (this.state.debts.length < 2){
-      alert("Please add at least 2 people");
-    } else {
-      location.assign('/bills/new');
-    }
-  }
-
+  // Post data to Rails api for batch creation and deletion
   handleSync(){
     let newCart = {
       items: this.state.items,
@@ -109,5 +114,17 @@ class MainCart extends React.Component{
     }).done(()=>{
       self.fetchServer();
     });
+  }
+
+  // Validations for navigating to new bill page
+  navigateBillNew(){
+    this.handleSync();
+    if (this.state.items.length === 0){
+      alert("Please add items");
+    } else if (this.state.debts.length < 2){
+      alert("Please add at least 2 people");
+    } else {
+      location.assign('/bills/new');
+    }
   }
 }
