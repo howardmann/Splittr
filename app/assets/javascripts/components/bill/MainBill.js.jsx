c932866d2f5891de920bc43dd86f3353fe09ecf4
howardmann <howardmann27@gmail.com>
2016-10-11 13:51:40 +1100

    Add and reduce bill item features added

--- app/assets/javascripts/components/bill/MainBill.js.jsx
+++ app/assets/javascripts/components/bill/MainBill.js.jsx
@@ -75,27 +75,32 @@ class MainBill extends React.Component {
     )
   }
 
+  // Decrease quantity of item that is clicked on
   updateReduce(property, id, user_id){
     if (property === "items") {
+      // Find BillItem state associated with clicked item id
       var itemIndex = findWithAttr(this.state[property],'id',id);
       var item = this.state[property][itemIndex];
     } else if (property === "debts") {
-      // TOdo Validate correct user
+      // Find debt associated with current user
       var debtIndex = findWithAttr(this.state[property],'user_id',this.state.current_user_id);
       var debt = this.state[property][debtIndex];
 
+      // Validate if this element belongs to current user
       if (debt.user_id != user_id) {
         console.log("Does not match current_user");
         return false;
       }
-
+      // Find DebtItem state associated with clicked item id
       var debtItemIndex = findWithAttr(debt.items, 'id', id)
       var item = debt.items[debtItemIndex];
     }
 
+    // Reduce item quantity and update subtotal
     item.quantity -= 1;
     item.subtotal = (item.quantity * parseInt(item.price)).toFixed(2);
 
+    // Delete if quantity falls below 0 and update state
     if (item.quantity === 0){
       this.updateDelete(property, id, debt, debtItemIndex);
     } else {
@@ -104,11 +109,11 @@ class MainBill extends React.Component {
       });
     }
 
-    if (property==="items"){
-      this.updateAdd(item);
-    }
+    // Callback to add corresponding Bill/ Debt item
+    this.updateAdd(property, item);
   }
 
+  // Callback to delete item if quantity below 0
   updateDelete(property, id, debt, debtItemIndex){
     var itemsArr;
 
@@ -128,31 +133,64 @@ class MainBill extends React.Component {
     }
   }
 
-  updateAdd(item){
-    var debtIndex = findWithAttr(this.state.debts,'user_id',this.state.current_user_id);
-    var debt = this.state.debts[debtIndex];
-    console.log(item,debt.name);
-    var debtItemIndex = findWithAttr(debt.items,'description',item.description);
-    var debtItem = debt.items[debtItemIndex];
-    if (debtItem) {
-      debtItem.quantity += 1;
-      debtItem.subtotal = (debtItem.quantity * parseInt(debtItem.price)).toFixed(2);
-    } else {
-      let newDebtItem = Object.assign({},item);
-      newDebtItem.quantity = 1;
-      newDebtItem.subtotal = (newDebtItem.quantity * parseInt(newDebtItem.price)).toFixed(2);
-      debt.items.push(newDebtItem);
-    }
+  // Callback to increment corresponding Bill/Debt item. Refactor later
+  updateAdd(property, item){
+    if (property === "items") {
+      // Find if same item belongs to a debt.user by checking description
+      var debtIndex = findWithAttr(this.state.debts,'user_id',this.state.current_user_id);
+      var debt = this.state.debts[debtIndex];
+      var debtItemIndex = findWithAttr(debt.items,'description',item.description);
+      var debtItem = debt.items[debtItemIndex];
 
-    this.setState({
-      debts: this.state.debts
-    })
+      // If debtItem already exists increment by one, otherwise create a new item for that debt.user and setState
+      if (debtItem) {
+        debtItem.quantity += 1;
+        debtItem.subtotal = (debtItem.quantity * parseInt(debtItem.price)).toFixed(2);
+      } else {
+        let newDebtItem = Object.assign({},item);
+        newDebtItem.quantity = 1;
+        newDebtItem.subtotal = (newDebtItem.quantity * parseInt(newDebtItem.price)).toFixed(2);
+        debt.items.push(newDebtItem);
+      }
+      this.setState({
+        debts: this.state.debts
+      })
+    } else if (property === "debts") {
+      // Find item in bill with same description
+      var billItemIndex = findWithAttr(this.state.items,'description',item.description);
+      var billItem = this.state.items[billItemIndex];
+
+      // If item already exists increment by one, otherwise create a new item for that debt.user and setState
+      if (billItem) {
+        billItem.quantity += 1;
+        billItem.subtotal = (billItem.quantity * parseInt(billItem.price)).toFixed(2);
+      } else {
+        let newItem = Object.assign({},item);
+        newItem.quantity = 1;
+        newItem.id = Math.random();
+        newItem.subtotal = (newItem.quantity * parseInt(newItem.price)).toFixed(2);
+        this.state.items.push(newItem);
+      }
+      this.setState({
+        items: this.state.items
+      })
+    }
+    this.updateBillTotal();
   }
 
+  updateBillTotal(){
+    let updateTotal = this.state.items.reduce((sum,el)=>{
+      return sum += parseInt(el.subtotal);
+    },0);
+    this.setState({
+      total: parseInt(updateTotal).toFixed(2)
+    });
+  }
 }
 
 
 class BillDebt extends React.Component{
+
   render(){
     let items = this.props.debt.items.map((item)=>{
         return (
@@ -161,12 +199,20 @@ class BillDebt extends React.Component{
         )
     });
 
+    let total = this.props.debt.items.reduce((sum,item)=>{
+      return sum += parseInt(item.subtotal);
+    },0);
+
     return (
       <tbody>
         <tr className="username">
           <td colSpan="4">{this.props.debt.name}</td>
         </tr>
         {items}
+        <tr>
+          <td colSpan="3"><strong>Total</strong></td>
+          <td><strong>{total.toFixed(2)}</strong></td>
+        </tr>
       </tbody>
     )
   }
