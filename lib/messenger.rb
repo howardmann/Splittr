be68c4aa5c76b196aa567c57480a440c9754fc7c
howardmann <howardmann27@gmail.com>
2016-10-12 21:53:44 +1100

    Twilio ignore error message and puts in console

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -5,16 +5,20 @@ module Messenger
     account_sid = ENV["TWILIO_ACCOUNT_SID"]
     auth_token = ENV["TWILIO_AUTH_TOKEN"]
 
-    @client = Twilio::REST::Client.new account_sid, auth_token
+    begin
+      @client = Twilio::REST::Client.new account_sid, auth_token
 
-    number[0] = ""
-    msg = "Welcome #{user.name} to Splittr, you have been added to a new bill. Visit the link to check your bills https://bill-splittr.herokuapp.com/login"
+      number[0] = ""
+      msg = "Welcome #{user.name} to Splittr, you have been added to a new bill. Visit the link to check your bills https://bill-splittr.herokuapp.com/login"
 
-    @message = @client.account.messages.create(
-      from: "61476856534",
-      to: number,
-      body: msg
-    )
+      @message = @client.account.messages.create(
+        from: "61476856534",
+        to: number,
+        body: msg
+      )
+    rescue Twilio::REST::RequestError => e
+      puts e.message
+    end
   end
 
 end
